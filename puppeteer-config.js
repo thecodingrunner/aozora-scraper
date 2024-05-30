045d45b938071b737725df0e6a80b539903af7d5
thecodingrunner <finn.moffett@gmail.com>
2024-05-30 15:18:57 +0100

    Add aws chrome lambda

---
+++ puppeteer-config.js
@@ -0,0 +1,16 @@
+import { join } from 'path';
+import chromium from 'chrome-aws-lambda';
+
+const isServerless = process.env.VERCEL || process.env.AWS_LAMBDA_FUNCTION_NAME;
+
+const puppeteerConfig = async () => ({
+    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+    executablePath: isServerless 
+        ? await chromium.executablePath
+        : join('C:', 'Users', 'Finn', 'repos', 'book-analysis-app', '.cache', 'puppeteer', 'chrome', 'win64-125.0.6422.78', 'chrome-win64', 'chrome.exe'),
+    args: isServerless ? chromium.args : [],
+    defaultViewport: chromium.defaultViewport,
+    headless: isServerless ? chromium.headless : true,
+});
+
+export default puppeteerConfig;
