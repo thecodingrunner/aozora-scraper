5578d168605f6ed686989cf6d5de54aa93816f0a
thecodingrunner <finn.moffett@gmail.com>
2024-05-30 14:02:27 +0100

    Added executablePath

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -6,4 +6,5 @@ const {join} = require('path');
 module.exports = {
     // Changes the cache location for Puppeteer.
     cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+    executablePath: join(__dirname, '.cache', 'puppeteer', 'chrome', 'win63.125.0.6422.78', 'chrome-win64'),
 };
